925b9d2fdcb2db3f95ca6b68123fd9d25de54f29
ICDTAS <ICDATS@qq.com>
2022-09-10 09:18:47 +0800

    third commit by hxj

--- src/com/pta/Text.java
+++ src/com/pta/Text.java
@@ -10,5 +10,6 @@ public class Text {
         strings.add("hello");
         System.out.println(strings);
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
